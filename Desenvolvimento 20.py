43202a8da09c73b0ca45f78927199eae54e1a6fd
mayeufraferreira <108952622+mayeufraferreira@users.noreply.github.com>
2022-07-30 18:12:54 -0300

    Add files via upload

---
+++ Desenvolvimento 20.py
@@ -0,0 +1,20 @@
+import pandas as pd
+df = pd.read_csv ("C:/Users/mayka/OneDrive/Documentos/notas_alunos.csv")
+media = (df["nota_1"] + df["nota_2"])/2
+
+df["média"] = media
+
+df.loc[df["média"] >=7, "situação"] = "APROVADO"
+df.loc[df["média"] <7, "situação"] = "REPROVADO"
+df.loc[df["faltas"] >5, "situação"] = "REPROVADO"
+
+df.to_csv("C:/Users/mayka/OneDrive/Documentos/alunos_situacao.csv")
+
+maiorNumFaltas = df["faltas"].max()
+print("O maior números de faltas é:",(maiorNumFaltas))
+
+mediaGeral = df["média"].median()
+print("A média geral das notas é:",(mediaGeral))
+
+maiorMedia = df["média"].max()
+print("A maior média é:",(maiorMedia))
