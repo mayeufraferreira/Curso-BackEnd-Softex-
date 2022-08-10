b364d9adc5affcd40912e0a6199bad661c5f7b91
mayeufraferreira <108952622+mayeufraferreira@users.noreply.github.com>
2022-08-10 16:10:06 -0300

    Update Desenvolvimento 20.py

--- Desenvolvimento 20.py
+++ Desenvolvimento 20.py
@@ -5,15 +5,14 @@ media = (df["nota_1"] + df["nota_2"])/2
 df["média"] = media
 
 df.loc[df["média"] >=7, "situação"] = "APROVADO"
-df.loc[df["média"] <7, "situação"] = "REPROVADO"
-df.loc[df["faltas"] >5, "situação"] = "REPROVADO"
+df.loc[df["média"] <7 | df["faltas"] >5, "situação"] = "REPROVADO"
 
 df.to_csv("C:/Users/mayka/OneDrive/Documentos/alunos_situacao.csv")
 
 maiorNumFaltas = df["faltas"].max()
 print("O maior números de faltas é:",(maiorNumFaltas))
 
-mediaGeral = df["média"].median()
+mediaGeral = df["média"].mean()
 print("A média geral das notas é:",(mediaGeral))
 
 maiorMedia = df["média"].max()
