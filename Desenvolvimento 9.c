c1f05876db7cf24546a7370958d206503113501c
mayeufraferreira <108952622+mayeufraferreira@users.noreply.github.com>
2022-08-12 16:21:03 -0300

    Add files via upload

---
+++ Desenvolvimento 9.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int *ptr = (int*) malloc(11 *sizeof(int));
+    int *ptr = (int*) realloc(ptr, 22 *sizeof(int));
+    free(ptr);
+    return 0;
+}
