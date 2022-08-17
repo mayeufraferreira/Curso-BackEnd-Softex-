b1f5bc81536c569fe15f12c480a9d9c55e2dd390
mayeufraferreira <108952622+mayeufraferreira@users.noreply.github.com>
2022-08-17 15:50:59 -0300

    Update Desenvolvimento 9.c

--- Desenvolvimento 9.c
+++ Desenvolvimento 9.c
@@ -3,8 +3,8 @@
 
 int main()
 {
-    int *ptr = (int*) malloc(11 *sizeof(int));
-    int *ptr = (int*) realloc(ptr, 22 *sizeof(int));
+    int *ptr = (int*) malloc(11 * sizeof(int));
+    int *ptr = (int*) realloc(ptr, 22 * sizeof(int));
     free(ptr);
     return 0;
 }
