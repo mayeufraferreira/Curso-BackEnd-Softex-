d17e7990c2cec581190d58906254585b8e811261
mayeufraferreira <108952622+mayeufraferreira@users.noreply.github.com>
2022-08-19 11:51:40 -0300

    Update Desenvolvimento 9.c

--- Desenvolvimento 9.c
+++ Desenvolvimento 9.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    int *ptr = (int*) malloc(11 * sizeof(int));
-    int *ptr = (int*) realloc(ptr, 22 * sizeof(int));
-    free(ptr);
-    return 0;
+int main() {
+    int *v = (int*) malloc(11 * sizeof(int));
+    *v = (int*) realloc(v, 22 * sizeof(int));
+    
+    for (int i = 0; i < 22; i++){
+        v[i] = 2 * i;
+    }
+    
+    for (int i = 0; i < 22; i++) {
+        printf("%d\n", v[i]);
+    }
+    
+    free(v);
 }
