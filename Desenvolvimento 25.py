9e8d67038f98dfc98e48a3f3e1cb6602a84e94b2
mayeufraferreira <108952622+mayeufraferreira@users.noreply.github.com>
2022-08-23 20:52:19 -0300

    Add files via upload

---
+++ Desenvolvimento 25.py
@@ -0,0 +1,6 @@
+frase = "Curso de lógica de programação e orientação a objetos"
+
+print("Frase inicial:", frase)
+print("Em maiúsculas:", frase.upper())
+print("Em minúsculas:", frase.lower())
+print("Apenas a primeira letra maiúscula:", frase.title())
