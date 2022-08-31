fe61a5d0c6d48a6f39d71896109a2312b3f2b800
mayeufraferreira <108952622+mayeufraferreira@users.noreply.github.com>
2022-08-31 19:45:29 -0300

    Add files via upload

---
+++ Desenvolvimento 24/BoasVindas.java
@@ -0,0 +1,11 @@
+
+package br.com.techcompany.login.boasvindas;
+
+
+public class BoasVindas {
+    public static void main(String[] args) {
+        System.out.println("Seja Bem Vindo(a)!");
+      
+    }
+    
+}
---
+++ Desenvolvimento 24/Login.java
@@ -0,0 +1,14 @@
+
+
+package br.com.techcompany.login;
+
+import javax.swing.JOptionPane;
+
+
+public class Login {
+
+    public static void main(String[] args) {
+        String email = JOptionPane.showInputDialog("Email:");
+        String senha = JOptionPane.showInputDialog("Senha:");
+    }
+}
