1ceb25ccd12905fa1ad25310d13edb92a077af39
mayeufraferreira <108952622+mayeufraferreira@users.noreply.github.com>
2022-09-01 18:00:00 -0300

    Add files via upload

---
+++ Desenvolvimento 27.java
@@ -0,0 +1,20 @@
+
+package com.mayara.excecoes;
+
+import javax.swing.JOptionPane;
+
+public class Excecoes {
+   public static void main (String[] args){
+       try {
+           int num1 = Integer.parseInt(JOptionPane.showInputDialog("Primeiro número:"));
+           int num2 = Integer.parseInt(JOptionPane.showInputDialog("Segundo número:"));
+           int div = num1/num2;
+           JOptionPane.showMessageDialog(null,"Resultado da sua divisão: " + div);
+       
+       } catch (ArithmeticException e) {
+           JOptionPane.showMessageDialog(null,"Não é possível dividir um número por 0, tente novamente");
+           
+       }
+   } 
+}
+
