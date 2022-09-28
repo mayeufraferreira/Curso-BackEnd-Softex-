199bca4ded34a9f1c2986437c5826a0ac2b6e367
mayeufraferreira <108952622+mayeufraferreira@users.noreply.github.com>
2022-09-28 15:23:37 -0300

    Update Desenvolvimento 13.py

--- Desenvolvimento 13.py
+++ Desenvolvimento 13.py
@@ -4,10 +4,8 @@ n2 = float(input ("Segunda nota: "))
 f = int(input("Número de faltas: "))
 m = (n1+n2)/2
 
-
 if m>=7 and f<=3:
    print("O(A) aluno(a)",nome,"foi APROVADO(A)!")
 else:
    print("O(A) aluno(a)",nome,"foi REPROVADO(A)!")
-   
-   
+  
