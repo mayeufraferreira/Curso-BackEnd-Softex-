23798cd46e44943bc23fca68e22cc10fa05de862
mayeufraferreira <108952622+mayeufraferreira@users.noreply.github.com>
2022-10-06 19:13:17 -0300

    Delete Des10 - JavaScript directory

--- Des10 - JavaScript/index.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const app = express();
-
-app.get('/', function (request, response) {
-    return response.send("Desenvolvimento do módulo 4 de JavaScript");
-});
-
-app.listen(8000);
